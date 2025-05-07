12e8325ad77baf695ab971e921fd8d5429bceabc
annacatherin-03 <24mca09@kgisliim.ac.in>
2025-05-07 15:10:36 +0530

    ava addon Problem solving Test. 06/05/2025

---
+++ PerfectNumbers.java
@@ -0,0 +1,18 @@
+
+package question02;
+public class PerfectNumbers {
+    public static void main(String[] args) {
+        System.out.print("Perfect numbers between 1 and 1000: ");
+        for (int num = 1; num <= 1000; num++) {
+            int sum = 0;
+            for (int i = 1; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    sum += i;
+                }
+            }
+            if (sum == num) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
